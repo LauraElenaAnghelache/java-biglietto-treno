b26b65dbd19ecdff2d242c8d1c11341176bbbbb5
LauraElenaAnghelache <laura.elena.anghelache@outlook.it>
2022-06-07 15:50:43 +0200

    first commit

---
+++ src/jana60/CalcolaBiglietto.java
@@ -0,0 +1,20 @@
+package jana60;
+
+import java.util.Scanner;
+
+public class CalcolaBiglietto {
+
+	public static void main(String[] args) {
+		int eta;
+		int kilometri;
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Inserisci età: ");
+	    eta = scan.nextInt();
+		System.out.println("Kilometri da percorrere: ");
+		kilometri = scan.nextInt();
+		
+
+	}
+
+}
