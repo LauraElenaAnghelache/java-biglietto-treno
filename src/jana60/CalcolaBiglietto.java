603e660c123000e9dd019f83577db6c7acc79a38
LauraElenaAnghelache <laura.elena.anghelache@outlook.it>
2022-06-07 16:58:26 +0200

    last commit

--- src/jana60/CalcolaBiglietto.java
+++ src/jana60/CalcolaBiglietto.java
@@ -1,5 +1,6 @@
 package jana60;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class CalcolaBiglietto {
@@ -7,14 +8,34 @@ public class CalcolaBiglietto {
 	public static void main(String[] args) {
 		int eta;
 		int kilometri;
-		Scanner scan = new Scanner(System.in);
+		try (Scanner scan = new Scanner(System.in)) {
+			System.out.println("Inserisci età: ");
+			eta = scan.nextInt();
+			System.out.println("Kilometri da percorrere: ");
+			kilometri = scan.nextInt();
+		}
+		System.out.println("Età:" + eta);
+		System.out.println("kilometri:" + kilometri);
 		
-		System.out.println("Inserisci età: ");
-	    eta = scan.nextInt();
-		System.out.println("Kilometri da percorrere: ");
-		kilometri = scan.nextInt();
+		float prezzoBiglietto = kilometri * 0.21f;
+		double prezzoScontatoGiovani = prezzoBiglietto - (prezzoBiglietto * 0.2);
+		double prezzoScontatoAnziani = prezzoBiglietto - (prezzoBiglietto * 0.4);
+		DecimalFormat df = new DecimalFormat("0.00");
 		
-
+		if(eta <=18) {
+			System.out.println("passegero categoria Giovani, prezzo biglietto: " + df.format(prezzoScontatoGiovani));
+			}
+		else if(eta >=65) {
+			System.out.println("passegero categoria Anziani, prezzo biglietto: " + df.format(prezzoScontatoAnziani));
+		}
+		
+		System.out.println("Arrivederci");
+	
+				
 	}
-
 }
+		
+
+
+
+
